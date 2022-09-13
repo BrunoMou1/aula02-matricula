d7c3ad631adf31c8cecab99993fadfdf18bb8546
bruno moura <brunopmoura1@gmail.com>
2022-09-13 09:31:25 -0300

    ad scanners ao codigo

--- src/br/ufrn/imd/main/Main.java
+++ src/br/ufrn/imd/main/Main.java
@@ -1,67 +1,61 @@
 package br.ufrn.imd.main;
 
-import java.util.Calendar;
+import java.util.Scanner;
 
 import br.ufrn.imd.domino.Aluno;
 import br.ufrn.imd.domino.Curso;
 import br.ufrn.imd.domino.Disciplina;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		Aluno luiz = new Aluno();
+		Curso adm = new Curso();
+		Disciplina adm0169 = new Disciplina();
+		Disciplina adm0170 = new Disciplina();
 		
-		Aluno jose = new Aluno();
-		jose.setNome("José da Silva");
-		jose.setMatricula("20229393939");
-		
-		Calendar dataNascimento = Calendar.getInstance();
-		dataNascimento.set(Calendar.DATE, 19);
-		dataNascimento.set(Calendar.MONTH, 06);
-		dataNascimento.set(Calendar.YEAR, 1988);
-		jose.setDataNascimento(dataNascimento.getTime());
-		
-		Curso bti = new Curso();
-		bti.setDescricao("Bacharelado em Tecnologia da Informação");
-		
-		jose.setCurso(bti);
-		
-		Disciplina imd0040 = new Disciplina();
-		imd0040.setId(1);
-		imd0040.setCurso(bti);
-		imd0040.setDescricao("Linguagens de programação II");
-		
-		jose.solicitarMatricula(imd0040);
-		jose.setIra(9.6);
-		
-		Disciplina imd0404 = new Disciplina();
-		imd0404.setId(2);
-		imd0404.setCurso(bti);
-		imd0404.setDescricao("Web I");
-		
-		jose.solicitarMatricula(imd0404);
-		
-		System.out.println(jose);
-		
-		
-		for (Disciplina d : jose.listarDisciplinas()) {
-			System.out.println(d);
-		}
-		
-		Disciplina imd0404c = new Disciplina();
-		imd0404c.setId(2);
-		imd0404c.setCurso(bti);
-		imd0404c.setDescricao("Web I");
-		
-		jose.trancarMatricula(imd0404c);
-		
-		for (Disciplina d : jose.listarDisciplinas()) {
-			System.out.println(d);
-		}
-		
-		//inserção interativa desses itens 
-		//regras de negócio - implementação dos métodos de solicitar matrícula
-		
-	}
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o nome do aluno: ");
+		luiz.setNome(scanner.nextLine());
+		System.out.println("Digite a matricula do aluno: ");
+		luiz.setMatricula(scanner.nextLine());
+		System.out.println("Digite o cpf do aluno: ");
+		luiz.setCpf(scanner.nextLine());
+		System.out.println("Digite o ira do aluno: ");
+		luiz.setIra(scanner.nextDouble());
 
+				
+		System.out.println("Digite o nome do curso: ");
+		scanner.nextLine();
+		adm.setDescricao(scanner.nextLine());
+
+		luiz.setCurso(adm);
+
+		System.out.println("Digite a descriÃ§ao da disciplina: ");
+		adm0169.setDescricao(scanner.nextLine());
+		adm0169.setCurso(adm);
+		adm0169.setId(1);
+
+		luiz.solicitarMatricula(adm0169);
+
+		for (Disciplina d : luiz.listarDisciplinas()) {
+			System.out.println(d);
+		}
+
+		System.out.println("Digite a descriÃ§ao da disciplina: ");
+		adm0170.setDescricao(scanner.nextLine());
+		adm0170.setCurso(adm);
+		adm0170.setId(2);
+
+		luiz.solicitarMatricula(adm0170);
+		luiz.trancarMatricula(adm0169);
+
+		for (Disciplina d : luiz.listarDisciplinas()) {
+			System.out.println(d);
+		}
+
+		scanner.close();
+	}
 }
